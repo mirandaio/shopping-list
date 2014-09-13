e1bfd72662a9c3364d312bbfb4e619c84a3aa041
Alfredo Miranda <alfredo@miranda.io>
2014-09-12 23:43:13 -0500

    Add some functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 $(function() {
+  $('input').keydown(function(event) {
+    if(event.which == 13) {
+      var val = $(this).val();
+      $(this).val('');
+      $('.list').prepend('<div>' + val + '</div>');
+    }
+  });
 });
