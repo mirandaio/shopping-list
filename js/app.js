e423026331cafebd7a9a2a2055ca37bfdde65ee9
Alfredo Miranda <alfredo@miranda.io>
2014-09-13 17:13:02 -0500

    Add strike-out and delete functionality

--- js/app.js
+++ js/app.js
@@ -3,7 +3,15 @@ $(function() {
     if(event.which == 13) {
       var val = $(this).val();
       $(this).val('');
-      $('.list').prepend('<div>' + val + '</div>');
+      var item = $('<li><span class="check"></span>' + val + 
+        '<span class="close">x</span></li>');
+      item.find('.check').click(function() {
+        item.css('text-decoration', 'line-through');
+      });
+      item.find('.close').click(function() {
+        item.remove();
+      });
+      $('ul').prepend(item);
     }
   });
 });
